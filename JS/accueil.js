47d51674cbc95515591554e09ae849c476e12cd9
Estelle <[estou84@orange.fr]>
2019-01-03 10:41:43 +0100

    merge fichier accueil

--- JS/accueil.js
+++ JS/accueil.js
@@ -2,11 +2,71 @@ let formulaire = document.querySelector(".formulaire");
 let boutonJouer = document.querySelector("#boutonJouer");
 let dosCarte = document.getElementsByName("dosCarte");
 let animation = document.querySelector(".animation");
+let tableauDosCarte = [
+    document.getElementById('dosBlue'),
+    document.getElementById('dosBlack'),
+    document.getElementById('dosGreen'),
+    document.getElementById('dosRed'),
+    document.getElementById('dosYellow'),
+    document.getElementById('dosPurple')
+];
+let tableauCorrespondanceDos = ["blue_back", "Gray_back", "Green_back", "Red_back", "Yellow_back", "purple_back"];
+localStorage.setItem("choixDos", 0); // Selectionne le dos de carte bleue par défaut
+tableauDosCarte[localStorage.choixDos].style.transform = "translateY(-10px)";
+localStorage.dos = tableauCorrespondanceDos[localStorage.choixDos];
 let cagnotte = document.querySelector("#cagnotte");
 let alerteCagnotte = document.querySelector("#alerte");
-
 let alerte = "";
 
+function afficheSelectionDos() {
+    for (i = 0; i < tableauDosCarte.length; i++) {
+        tableauDosCarte[i].style.transform = "translateY(0px)";
+    }
+    tableauDosCarte[localStorage.choixDos].style.transform = "translateY(-10px)";
+    localStorage.dos = tableauCorrespondanceDos[localStorage.choixDos];
+}
+/**
+ * Selection du dos de carte
+ */
+
+// Selection du dos bleu
+dosBlue.addEventListener("click", function(){
+//     document.querySelector('.dos').addEventListener("click", function (e) {
+//         localStorage.dos = e.path[2].childElementCount - 1;
+//         console.log(e.path[2].childElementCount - 1);
+    localStorage.choixDos = 0;
+    afficheSelectionDos();
+});
+
+// Selection du dos gris
+document.getElementById('dosBlack').addEventListener("click", function () {
+    localStorage.choixDos = 1;
+    afficheSelectionDos();
+});
+
+// Selection du dos vert
+document.getElementById('dosGreen').addEventListener("click", function () {
+    localStorage.choixDos = 2;
+    afficheSelectionDos();
+});
+
+// Selection du dos rouge
+document.getElementById('dosRed').addEventListener("click", function () {
+    localStorage.choixDos = 3;
+    afficheSelectionDos();
+});
+
+// Selection du dos jaune
+document.getElementById('dosYellow').addEventListener("click", function () {
+    localStorage.choixDos = 4;
+    afficheSelectionDos();
+});
+
+// Selection du dos violet
+document.getElementById('dosPurple').addEventListener("click", function () {
+    localStorage.choixDos = 5;
+    afficheSelectionDos();
+});
 window.onload = function () {
     alerte = "";
     alerteCagnotte.innerHTML = alerte;
